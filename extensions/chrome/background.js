2945c4ab2288b7f40978241620db86d80d23fe5f
Tommy Calvy <42327902+tommycalvy@users.noreply.github.com>
2023-12-07 22:58:43 -0600

    broken sidepanel

--- extensions/chrome/background.js
+++ extensions/chrome/background.js
@@ -2,64 +2,70 @@ chrome.sidePanel
     .setPanelBehavior({ openPanelOnActionClick: true })
     .catch((error) => console.error(error));
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+/*
+chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
+    let tab = await getCurrentTab();
+    if (tabs[0].url?.startsWith("chrome://")) return undefined;
+    // TODO: Should 
     if (request.action === "startRecording") {
+        chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            files: ["record.js"],
+        });
+    }
+    if (request.action === "recordingStarted") {
+        if (tab.url?.startsWith("chrome://")) return undefined;
+        chrome.action.setBadgeText({ tabId: tab.id, text: "•REC" });
+        chrome.action.setBadgeBackgroundColor({ color: "#FE0000" });
+        
+    }
+    if (request.action === "recordingStopped") {
         chrome.tabs.query(
             { active: true, currentWindow: true },
             function (tabs) {
-                chrome.scripting.executeScript({
-                    target: { tabId: tabs[0].id },
-                    files: ["record.js"],
-                });
+                
+                chrome.action.setBadgeText({ tabId: tabs[0].id, text: "" });
             }
         );
     }
-    if (request.action === "recordingStarted") {
-        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            if (tabs[0].url?.startsWith("chrome://")) return undefined;
-            chrome.action.setBadgeText({ tabId: tabs[0].id, text: "•REC" });
-            chrome.action.setBadgeBackgroundColor({ color: "#FE0000" });
-        });
-    }
-    if (request.action === "recordingStopped") {
-        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            if (tabs[0].url?.startsWith("chrome://")) return undefined;
-            chrome.action.setBadgeText({ tabId: tabs[0].id, text: "" });
-        });
-    }
-});
-
-/*
-chrome.runtime.onConnect.addListener(port => {
-  port.onMessage.addListener(async msg => {
-
-    if (port.name === PortNames.SidePanelPort) {
-      if (msg.type === 'init') {
-        console.log('panel opened');
-
-        await storage.setItem('panelOpen', true);
-
-        port.onDisconnect.addListener(async () => {
-          await storage.setItem('panelOpen', false);
-          console.log('panel closed');
-          console.log('port disconnected: ', port.name);
-        });
-
-        const tab = await getCurrentTab();
-
-        if (!tab?.id) {
-          console.error("Couldn't get current tab");
-          return;
-        }
-
-        injectContentScript(tab.id);
-
-        port.postMessage({ 
-          type: 'handle-init', 
-          message: 'panel open' 
-        });
-      }
-    }
-  });
 });
 */
+
+chrome.runtime.onConnect.addListener((port) => {
+    port.onMessage.addListener(async (msg) => {
+        if (port.name === "sidepanel") {
+            if (msg.type === "init") {
+                console.log("panel opened");
+
+                await chrome.storage.sync.set({ panelOpen: true });
+
+                port.onDisconnect.addListener(async () => {
+                    await chrome.storate.sync.set({ panelOpen: false });
+                    console.log("panel closed");
+                    console.log("port disconnected: ", port.name);
+                });
+
+                port.postMessage({
+                    type: "handle-init",
+                    message: "panel open",
+                });
+            }
+        }
+    });
+});
+
+async function getCurrentTab() {
+    let queryOptions = { active: true, lastFocusedWindow: true };
+    // `tab` will either be a `tabs.Tab` instance or `undefined`.
+    let [tab] = await chrome.tabs.query(queryOptions);
+    return tab;
+}
+
+async function injectContentScript(tabId) {
+    console.log("injecting content script");
+
+    chrome.scripting.executeScript({
+        target: { tabId },
+        files: ["record.js"],
+    });
+}
