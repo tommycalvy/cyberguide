14f466f76345598624d6b6602af3768e6763cdd1
Tommy Calvy <42327902+tommycalvy@users.noreply.github.com>
2024-01-05 21:28:31 -0600

    rspack.config.js works. was able to make array of html plugins. inject false.

--- extension/rspack.config.js
+++ extension/rspack.config.js
@@ -5,23 +5,59 @@ const glob = require('glob');
 const isProduction = process.env.NODE_ENV === 'production';
 
 /** @type {import('@rspack/cli').Configuration} */
-    const config = {
-        context: __dirname,
-        mode: isProduction ? 'production' : 'development',
-        devtool: false,
-        entry: Object.fromEntries(
-            glob.sync(path.resolve(__dirname, 'src/**/*.js')).map((v) => [
-                v.split('src/')[1], v,
-            ]
-        )),
-        output: {
-            filename: "[name]",
-            path: path.resolve(__dirname, "dist"),
-        },
-        experiments: {
-            rspackFuture: {
-                disableApplyEntryLazily: true,
+const config = {
+    context: __dirname,
+    mode: isProduction ? 'production' : 'development',
+    devtool: false,
+    entry: Object.fromEntries(
+        glob.sync(path.resolve(__dirname, 'src/**/*.js')).map((v) => [
+            v.split('src/')[1], v,
+        ]
+    )),
+    output: {
+        filename: "[name]",
+        path: path.resolve(__dirname, "dist"),
+    },
+    plugins: glob.sync(path.resolve(__dirname, 'src/**/*.html')).map((v) => {
+        return new rspack.HtmlRspackPlugin({
+                template: v,
+                filename: v.split('src/')[1],
+                inject: false,
+        });
+    }),
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: {
+                                    tailwindcss: {},
+                                    autoprefixer: {},
+                                },
+                            },
+                        },
+                    },
+                ],
+                type: 'css',
             },
+        ],
+    },
+    experiments: {
+        rspackFuture: {
+            disableApplyEntryLazily: true,
         },
-    };
+    },
+};
 module.exports = config;
+/*
+[
+    new rspack.HtmlRspackPlugin({
+        template: "./src/popup/popup.html",
+        filename: "popup/popup.html",
+    })
+]
+* */
