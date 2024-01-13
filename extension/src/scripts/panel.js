a782c177fa6dcae923634bf442d2b484f5accbf3
Tommy Calvy <42327902+tommycalvy@users.noreply.github.com>
2024-01-13 16:41:43 -0600

    made widget look like a blue pill

--- extension/src/scripts/panel.js
+++ extension/src/scripts/panel.js
@@ -3,12 +3,15 @@ import browser from "webextension-polyfill";
 const shadowHost = document.createElement("div");
 const shadowRoot = shadowHost.attachShadow({ mode: "open" });
 shadowRoot.innerHTML = ` 
-    <div id="widget" class="absolute top-5 right-5 bg-sky-500 bg-opacity-90 
-        w-20 h-50 z-[20000] border-red-500 border-solid border-2">
-            <h1>Panel</h1>       
+    <div id="widget" class="fixed top-52 right-5 bg-cyan-400 bg-opacity-80 
+        w-20 min-h-72 z-[999999] flex items-center flex-col
+        justify-between pb-4 overflow-hidden
+        border-solid border-sky-800 border-4 rounded-full border-opacity-80">
+            <button id ="widget-move" class="w-full h-8 bg-white text-black
+                font-bold cursor-grab">M</button>
             <button id="widget-close" class="border-black 
                 bg-white hover:bg-gray-100 text-black font-bold
-                py-2 px-4 rounded">Close</button>
+                w-8 h-8 rounded-full">X</button>
     </div>
 `;
 
@@ -20,7 +23,7 @@ shadowRoot.adoptedStyleSheets = [stylesheet];
 
 document.body.appendChild(shadowHost);
 
-//const widget = shadowRoot.getElementById("widget");
+const widget = shadowRoot.getElementById("widget");
 const widgetClose = shadowRoot.getElementById("widget-close");
 if (widgetClose instanceof HTMLButtonElement) {
     widgetClose.addEventListener("click", () => {
@@ -34,4 +37,24 @@ if (widgetClose instanceof HTMLButtonElement) {
     console.error("panelClose is not an HTMLButtonElement");
 }
 console.log("panel opened");
+
+let isDragging = false;
+if (widget instanceof HTMLDivElement) {
+    widget.addEventListener('dragstart', function() {
+        isDragging = true;
+        console.log("dragstart");
+    });
+    widget.addEventListener('dragend', function() {
+        isDragging = false; 
+        console.log("dragend");
+    });
+    document.addEventListener('mousemove', function(e) {
+        if (isDragging) {
+            widget.style.top = e.pageY - widget.offsetHeight / 2 + 'px';
+            widget.style.left = e.pageX - widget.offsetWidth / 2 + 'px';
+        }
+    });
+}
+
+
 
