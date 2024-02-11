ae7e0700b922507e82ac5249868b7d95f745e0d1
Tommy Calvy <42327902+tommycalvy@users.noreply.github.com>
2024-02-10 21:47:22 -0600

    CyberGuide popup in solidjs

---
+++ vite-solid-crxjs/src/popup/Popup.tsx
@@ -0,0 +1,24 @@
+import type { Component } from 'solid-js';
+import { createSignal } from 'solid-js';
+import '../styles/modern-normalize.css';
+import '../styles/button.css';
+import styles from './Popup.module.css';
+
+const App: Component = () => {
+
+    return (
+        <div class={styles.Popup}>
+            <h1>CyberGuide</h1>
+            <div class={styles.buttons}>
+                <button class={styles.record}>Record</button>
+                <button class={styles.widget}>Widget</button>
+                <button class={styles.settings}>Settings</button>
+            </div>
+            <div>
+                <p>Version: 0.0.1</p>
+            </div>
+        </div>
+    );
+};
+
+export default App;
--- vite-solid-crxjs/src/popup/index.tsx
+++ vite-solid-crxjs/src/popup/index.tsx
@@ -2,7 +2,7 @@
 import { render } from 'solid-js/web';
 
 import './index.css';
-import App from './App';
+import App from './Popup';
 
 const root = document.getElementById('root');
 
