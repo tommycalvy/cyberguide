9016fe34dae7ef7bd8c57129e98a5d13f222e773
Tommy Calvy <42327902+tommycalvy@users.noreply.github.com>
2024-04-17 22:17:05 -0500

    starting anew with pnpm. Kept .gitignore this time

--- jest.preset.js
+++
@@ -1,8 +0,0 @@
-/** @type import('jest').Config*/
-const config = {
-    testEnvironment: 'jsdom',
-    testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
-    moduleFileExtensions: ['js', 'jsx'],
-    coverageReporters: ['html'],
-};
-module.exports = config;
